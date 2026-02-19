e54cef9bb89eb14e28218651821b8bc296ff037e
Obadah Almheidat <shop@abayaplaza.com>
2026-02-20 02:15:30 +0300

    force env reload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const OpenAI = require("openai");
-require("dotenv").config();
+require("dotenv").config({ override: true });
 
 const app = express();
 
